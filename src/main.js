480415975dce5ebcedf7f2fbbb44a924ee1b7385
Yangholmes <yangholmes@126.com>
2017-05-25 17:54:41 +0800

    making board module

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Yang from 'yangjs'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+
 import App from './App.vue'
 
 Vue.use(VueRouter)
 Vue.use(Yang)
+Vue.use(ElementUI)
 
 import routes from './routes.js'
 const router = new VueRouter({
